fa7dc961adbdb5cbab6127b6a7929a0f8af304b0
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2020-05-20 11:32:24 +0530

    Update mail_function.php

--- mail_function.php
+++ mail_function.php
@@ -6,19 +6,20 @@ require 'vendor/autoload.php';
 
         
 
+        // Mail/SMS agent
         $message_body= "One Time Password for login authentication:<br><br>".$otp;
 
         $mail = new PHPMailer();
         $mail->isSMTP();
         $mail->Host='smtp.gmail.com';
         $mail->SMTPAuth= true;
-        $mail->Username= 'prajwalkulkarni76@gmail.com';
+        $mail->Username= 'username';
         $mail->Password='password';
         $mail->SMTPSecure='tls';
         $mail->Port = 587;
         $mail->isHTML(true);
-        $mail->AddReplyTo('prajwalkulkarni76@gmail.com','Splitster Inc.');
-        $mail->SetFrom('prajwalkulkarni76@gmail.com','Splister Inc.');
+        $mail->AddReplyTo('to@gmail.com','Splitster Inc.');
+        $mail->SetFrom('from@gmail.com','Splister Inc.');
         $mail->AddAddress($email);
         $mail->Subject = "OTP to Login";
         $mail->MsgHTML($message_body);
